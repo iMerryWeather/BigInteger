3e9b3cc2becb971fbfe3e271cfb44a0d3629179f
Louis Shen <louiseshen@protonmail.com>
2021-11-18 12:45:19 +0800

    add compareMag(mag1:_,mag2:_)

---
+++ Sources/BigInteger/Utils.swift
@@ -0,0 +1,26 @@
+extension BigInteger {
+    /*
+     * (check leading zeros first)
+     * 1) Longer is bigger
+     * 2) if both have the same length, then compare from index 0.
+     *
+     * return true if mag1 ≥ mag2
+     */
+    private func compareMag(mag1 : [UInt32], mag2 : [UInt32]) -> Bool {
+        let mag1 = removeLeadingZeros(mag: mag1)
+        let mag2 = removeLeadingZeros(mag: mag2)
+
+        //Longer is bigger
+        if mag1.count != mag2.count {
+            return mag1.count > mag2.count
+        }
+
+        //compare from 0 to count
+        for i in 0 ..< mag1.count {
+            if mag1[i] < mag2[i] {
+                return false
+            }
+        }
+        return true
+    }
+}
