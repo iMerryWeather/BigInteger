2e8d51b9309901ec5bc81bc80e27dbb3dc934969
Louis Shen <louiseshen@protonmail.com>
2022-01-25 20:16:54 +0800

    add mod

---
+++ Sources/BigInteger/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Louis Shen on 2022/1/25.
+//
+
+import Foundation
---
+++ Sources/BigInteger/Modulus.swift
@@ -0,0 +1,22 @@
+//
+//  Modulus.swift
+//  
+//
+//  Created by Louis Shen on 2022/1/25.
+//
+
+extension BigInteger {
+    //Currently we consider that r is non-negative number.
+    private static func mod(lhs : BigInteger, rhs : BigInteger)
+                        -> BigInteger {
+        return BigInteger(signum: true,
+                              mag: divide(mag1: lhs.mag, mag2: rhs.mag).1)
+    }
+}
+
+//Operator wrappers %
+extension BigInteger {
+    public static func % (lhs : BigInteger, rhs : BigInteger) -> BigInteger {
+        return mod(lhs: lhs, rhs: rhs)
+    }
+}
