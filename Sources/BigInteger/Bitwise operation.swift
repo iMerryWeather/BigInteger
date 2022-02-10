86a53c91c504b6bd58c6c714487b1d45338898c7
Louis Shen <louiseshen@protonmail.com>
2022-02-10 17:23:38 +0800

    add

--- Sources/BigInteger/Bitwise operation.swift
+++ Sources/BigInteger/Bitwise operation.swift
@@ -40,6 +40,40 @@ extension BigInteger {
     }
 }
 
+extension BigInteger {
+    /*
+     * Returns a BigInteger whose value is lhs | rhs.  (This function returns
+     * a negative BigInteger if and only if either this or val is negative.)
+     */
+    private static func or(lhs : BigInteger, rhs : BigInteger) -> BigInteger {
+        var lhs = lhs
+        var rhs = rhs
+        var res = [UInt32]()
+        let count = max(lhs.mag.count, rhs.mag.count)
+        if !lhs.signum {
+            lhs.mag.twosComplement()
+        }
+        if !rhs.signum {
+            rhs.mag.twosComplement()
+        }
+        for i in 0 ..< count {
+            res.append(lhs.mag[i] | rhs.mag[i])
+        }
+        if lhs.signum && rhs.signum {
+            return BigInteger(signum: true, mag: res)
+        }
+        res.twosComplement()
+        return BigInteger(signum: false, mag: res)
+    }
+}
+
+//Operator wrappers &
+extension BigInteger {
+    public static func | (lhs : BigInteger, rhs : BigInteger) -> BigInteger {
+        return or(lhs: lhs, rhs: rhs)
+    }
+}
+
 /*
  * Return two's-complement representation of this array, do not use on sigum is
  * TRUE
