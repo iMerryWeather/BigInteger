36c0a4abd97fd4bad43e2d3d09e64c7ec919969f
Louis Shen <louiseshen@protonmail.com>
2022-02-21 15:52:33 +0800

    (test) merge add test

--- Tests/BigIntegerTests/BigIntegerTests.swift
+++ Tests/BigIntegerTests/BigIntegerTests.swift
@@ -3,10 +3,39 @@ import XCTest
 import PythonKit
 
 final class BigIntegerTests: XCTestCase {
-    let numA2048 = getRandomNum(withCount: 2048)
-    let numB2048 = getRandomNum(withCount: 2048)
-    let numA256 = getRandomNum(withCount: 256)
-    let numA20480 = getRandomNum(withCount: 20480)
+    static let numA2048 = getRandomNum(withCount: 2048)
+    static let _numA2048 = "-" + numA2048
+    static let numB2048 = getRandomNum(withCount: 2048)
+    static let _numB2048 = "-" + numB2048
+    static let numA16 = getRandomNum(withCount: 16)
+    static let _numA16 = "-" + numA16
+    static let numB16 = getRandomNum(withCount: 16)
+    static let _numB16 = "-" + numB16
+    static let numA20480 = getRandomNum(withCount: 20480)
+    
+    //2048 digits long big integer A
+    let bigIntA2048 = BigInteger(from: numA2048)
+    let pyBigIntA2048 = Python.int(numA2048.pythonObject)
+    let _bigIntA2048 = BigInteger(from: _numA2048)
+    let _pyBigIntA2048 = Python.int(_numA2048.pythonObject)
+    
+    //2048 digits long big integer B
+    let bigIntB2048 = BigInteger(from: numB2048)
+    let pyBigIntB2048 = Python.int(numB2048.pythonObject)
+    let _bigIntB2048 = BigInteger(from: _numB2048)
+    let _pyBigIntB2048 = Python.int(_numB2048.pythonObject)
+    
+    //16 digits long big integer A
+    let bigIntA16 = BigInteger(from: numA16)
+    let intA16 = Int(numA16)!
+    let _bigIntA16 = BigInteger(from: _numA16)
+    let _intA16 = -1 * Int(numA16)!
+    
+    //16 digits long big integer B
+    let bigIntB16 = BigInteger(from: numB16)
+    let intB16 = Int(numB16)!
+    let _bigIntB16 = BigInteger(from: _numB16)
+    let _intB16 = -1 * Int(numB16)!
     
     func testPublicInitFromStr() {
         let a = BigInteger(from:
@@ -23,65 +52,6 @@ final class BigIntegerTests: XCTestCase {
         XCTAssertEqual(c.signum, true)
     }
 
-    func testAddMagT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "61935861237")
-        //c_mag = [3138768373, 3505532565, 28376639]
-        let c = BigInteger(from: "523456612361193061935861237")
-        //d_mag = [1946157056, 1340620830, 27105054]
-        let d = BigInteger(from: "500000000000000000000000000")
-        XCTAssertEqual(BigInteger.add(mag1: a.mag, mag2: b.mag),
-                       [2931821546, 842529975, 33])
-        XCTAssertEqual(BigInteger.add(mag1: c.mag, mag2: c.mag),
-                       [1982569450, 2716097835, 56753279])
-        XCTAssertEqual(BigInteger.add(mag1: d.mag, mag2: d.mag),
-                       [3892314112, 2681241660, 54210108])
-
-    }
-
-    func testSubtractMagT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "61935861237")
-        let c = BigInteger(from: "523456612361193061935861237")
-        let d = BigInteger(from: "500000000000000000000000000")
-        XCTAssertEqual(BigInteger.subtract(mag1: a.mag, mag2: b.mag),
-                       [3614150656, 842529946, 33])
-        XCTAssertEqual(BigInteger.subtract(mag1: c.mag, mag2: d.mag),
-                       [1192611317, 2164911735, 1271585])
-        XCTAssertEqual(BigInteger.subtract(mag1: d.mag, mag2: d.mag), [0])
-
-    }
-
-    func testMulMagT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "61935861237")
-        let c = BigInteger(from: "523456612361193061935861237")
-        let d = BigInteger(from: "500000000000000000000000000")
-
-        XCTAssertEqual(BigInteger.multiply(mag1: a.mag, mag2: b.mag),
-                       [2443410553, 2573930479, 3038812614, 478])
-        XCTAssertEqual(BigInteger.multiply(mag1: c.mag, mag2: d.mag),
-            [67108864, 2044281865, 1819053428, 197960665, 3325078967, 179081])
-        XCTAssertEqual(BigInteger.multiply(mag1: c.mag, mag2: [0]), [0])
-    }
-
-    func testDivMagT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "61935861237")
-        let c = BigInteger(from: "523456612361193061935861237")
-        let d = BigInteger(from: "500000000000000000000000000")
-
-        //hack, should be fixed in division(mag1:_, mag2:_) v1.1b1
-        let e = BigInteger(from: "612361193061935861237324234354353452352542352435435252435453")
-        let f = BigInteger(from: "1000000000000000000")
-
-        XCTAssertEqual(BigInteger.divide(mag1: e.mag, mag2: f.mag).0, [4274521118, 267499566, 3951190577, 2438715792, 1799, 0])
-        XCTAssertEqual(BigInteger.divide(mag1: a.mag, mag2: b.mag).0, [1297086930, 2])
-        XCTAssertEqual(BigInteger.divide(mag1: a.mag, mag2: b.mag).1, [2341514747, 6])
-        XCTAssertEqual(BigInteger.divide(mag1: c.mag, mag2: d.mag).0, [1])
-        XCTAssertEqual(BigInteger.divide(mag1: c.mag, mag2: d.mag).1, [1192611317, 2164911735, 1271585])
-    }
-
     func testStringConvertible() {
         let a = BigInteger(from: "612361193061935861237")
         XCTAssertEqual(String(a), "612361193061935861237")
@@ -89,63 +59,38 @@ final class BigIntegerTests: XCTestCase {
         XCTAssertEqual(String(b), "-612361193061935861237324234354353452352542352435435252435453")
         XCTAssertEqual(String(BigInteger(from: "0")), "0")
     }
-
-    func testAddT1() {
-        let a = BigInteger(from: "0000000000000000000000000000000000000612361193061935861237")
-        let b = BigInteger(from: "-6123611930619358612362")
-        let c = BigInteger(from: "-612361193061935861237")
-        let d = BigInteger(from: "6123611930619358612362")
-
-        XCTAssertEqual((a + a).mag, BigInteger(from: "1224722386123871722474").mag)
-        XCTAssertEqual((a + a).signum, BigInteger(from: "1224722386123871722474").signum)
-
-        XCTAssertEqual(String(a + b), "-5511250737557422751125")
-        XCTAssertEqual(String(c + d), "5511250737557422751125")
-    }
-
-    func testSubtractT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "-6123611930619358612362")
-        let c = BigInteger(from: "-612361193061935861237")
-        let d = BigInteger(from: "6123611930619358612362")
-
-        XCTAssertEqual(String(a - a), "0")
-        XCTAssertEqual(String(a - b), "6735973123681294473599")
-
-        XCTAssertEqual(String(c - c), "0")
-        XCTAssertEqual(String(c - d), "-6735973123681294473599")
-    }
-
-    func testMultiplyT1() {
-        let a = BigInteger(from: "612361193061935861237")
-        let b = BigInteger(from: "-6123611930619358612362")
-        XCTAssertEqual(String(a * b), "-3749862307682374847564897134347193704811794")
-        XCTAssertEqual(String(a * a), "374986230768237484756979602389168919170169")
-        XCTAssertEqual(String(a * BigInteger(from: "0")), "0")
-    }
-
-    func testDivideT1() {
-        let a = BigInteger(from: "-612361193061935861237")
-        let b = BigInteger(from: "6123611930619358612362")
-        XCTAssertEqual(String(a / a), "1")
-        XCTAssertEqual(String(a / b), "0")
-        XCTAssertEqual(String(b / a), "-9")
-    }
     
-    func testAddT2() {
-        let a = BigInteger(from: numA2048)
-        let b = BigInteger(from: numB2048)
-        let minusB = BigInteger(from: "-" + numB2048)
-
-        let x : PythonObject = numA2048.pythonObject
-        let y : PythonObject = numB2048.pythonObject
-        XCTAssertEqual(String(a + b), String(Python.int(x) + Python.int(y)))
-        XCTAssertEqual(String(a + minusB), String(Python.int(x) - Python.int(y)))
+    func testAdd() {
+        //small case, a > 0 && b > 0
+        XCTAssertEqual(String(bigIntA16 + bigIntB16),
+                       String(intA16 + intB16))
+        //small case, a > 0 && b < 0
+        XCTAssertEqual(String(bigIntA16 + _bigIntB16),
+                       String(intA16 + _intB16))
+        //small case, a < 0 && b > 0
+        XCTAssertEqual(String(_bigIntA16 + bigIntB16),
+                       String(_intA16 + intB16))
+        //small case, a < 0 && b < 0
+        XCTAssertEqual(String(_bigIntA16 + _bigIntB16),
+                       String(_intA16 + _intB16))
+        
+        //big case, a > 0 && b > 0
+        XCTAssertEqual(String(bigIntA2048 + bigIntB2048),
+                       String(pyBigIntA2048 + pyBigIntB2048))
+        //big case, a > 0 && b < 0
+        XCTAssertEqual(String(bigIntA2048 + _bigIntB2048),
+                       String(pyBigIntA2048 + _pyBigIntB2048))
+        //big case, a < 0 && b > 0
+        XCTAssertEqual(String(_bigIntA2048 + bigIntB2048),
+                       String(_pyBigIntA2048 + pyBigIntB2048))
+        //big case, a < 0 && b < 0
+        XCTAssertEqual(String(_bigIntA2048 + _bigIntB2048),
+                       String(_pyBigIntA2048 + _pyBigIntB2048))
     }
-    
+    /*
     func testSubtractT2() {
-        let a = BigInteger(from: numA2048)
-        let b = BigInteger(from: numB2048)
+        let a = BigInteger(from: BigIntegerTests.numA2048)
+        let b = BigInteger(from: BigIntegerTests.numB2048)
         let ma = BigInteger(from: "-" + numA2048)
         let mb = BigInteger(from: "-" + numB2048)
         var x : PythonObject = numA2048.pythonObject
@@ -286,7 +231,7 @@ final class BigIntegerTests: XCTestCase {
         XCTAssertEqual(String(b >> 1935800),
                        String(Python.divmod(Python.int(y),
                               Python.pow(2, 1935800)).tuple2.0))
-    }
+    }*/
     
     //Return a n size positive test number
     static func getRandomNum(withCount n : Int) -> String {
