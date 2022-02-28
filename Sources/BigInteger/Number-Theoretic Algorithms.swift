1f2f0d5f83125d608b195acb84848ac53aedbbb5
Louis Shen <louiseshen@protonmail.com>
2022-02-28 17:25:55 +0800

    add gcd()

--- Sources/BigInteger/Number-Theoretic Algorithms.swift
+++ Sources/BigInteger/Number-Theoretic Algorithms.swift
@@ -1,8 +1,31 @@
 //
-//  File.swift
+//  Number-Theoretic Algorithms.swift
 //  
 //
 //  Created by Louis Shen on 2022/2/28.
 //
 
-import Foundation
+extension BigInteger {
+    /*
+     * Returns a BigInteger whose value is the greatest common divisor of
+     * abs(lhs) and abs(rhs).  Returns 0 if lhs == 0 && rhs == 0.
+     *
+     * Euclid's Algorithm
+     *   gcd(a, b) = gcd(b, a mod b)
+     *   gcd(a, b) = gcd(|a|, |b|)
+     *
+     * See CLRS, _Introduction to Algorithms_, 3e, (31.8, Thm. 31.9)
+     */
+    public static func gcd(_ lhs: BigInteger, _ rhs: BigInteger) -> BigInteger {
+        return euclid(lhs.abs(), rhs.abs())
+    }
+    
+    private static func euclid(_ a : BigInteger,
+                               _ b : BigInteger) -> BigInteger {
+        if b == BigInteger.ZERO {
+            return a
+        } else {
+            return euclid(b, a % b)
+        }
+    }
+}
---
+++ Tests/BigIntegerTests/NT-algoTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+@testable import BigInteger
+import PythonKit
+
+final class NT_algoTests : XCTestCase {
+    static let numA2048 = BigIntegerTests.getRandomNum(withCount: 2048)
+    static let _numA2048 = "-" + numA2048
+    static let numB2048 = BigIntegerTests.getRandomNum(withCount: 2048)
+    static let _numB2048 = "-" + numB2048
+    static let numA16 = BigIntegerTests.getRandomNum(withCount: 16)
+    static let _numA16 = "-" + numA16
+    static let numB16 = BigIntegerTests.getRandomNum(withCount: 16)
+    static let _numB16 = "-" + numB16
+    static let numA20480 = BigIntegerTests.getRandomNum(withCount: 20480)
+    
+    //2048 digits long big integer A
+    let bigIntA2048 = BigInteger(from: numA2048)
+    let pyBigIntA2048 = Python.int(numA2048.pythonObject)
+    let _bigIntA2048 = BigInteger(from: _numA2048)
+    let _pyBigIntA2048 = Python.int(_numA2048.pythonObject)
+    
+    //2048 digits long big integer B
+    let bigIntB2048 = BigInteger(from: numB2048)
+    let pyBigIntB2048 = Python.int(numB2048.pythonObject)
+    let _bigIntB2048 = BigInteger(from: _numB2048)
+    let _pyBigIntB2048 = Python.int(_numB2048.pythonObject)
+    
+    //16 digits long big integer A
+    let bigIntA16 = BigInteger(from: numA16)
+    let intA16 = Int(numA16)!
+    let pyBigIntA16 = Python.int(numA16.pythonObject)
+    let _bigIntA16 = BigInteger(from: _numA16)
+    let _intA16 = Int(_numA16)!
+    let _pyBigIntA16 = Python.int(_numA16.pythonObject)
+    
+    //16 digits long big integer B
+    let bigIntB16 = BigInteger(from: numB16)
+    let intB16 = Int(numB16)!
+    let pyBigIntB16 = Python.int(numB16.pythonObject)
+    let _bigIntB16 = BigInteger(from: _numB16)
+    let _intB16 = Int(_numB16)!
+    let _pyBigIntB16 = Python.int(_numB16.pythonObject)
+    
+    func testGCD() {
+        let py_math = Python.import("math")
+        XCTAssertEqual(String(BigInteger.gcd(BigInteger.ZERO, BigInteger.ZERO)),
+                       String(0))
+        XCTAssertEqual(String(BigInteger.gcd(bigIntA2048, bigIntB2048)),
+                       String(py_math.gcd(pyBigIntA2048, pyBigIntB2048)))
+    }
+}
