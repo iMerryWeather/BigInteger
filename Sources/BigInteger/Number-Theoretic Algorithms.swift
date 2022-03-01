0890c6afce47d186b8810fd9248276a39bb6845b
Louis Shen <louiseshen@protonmail.com>
2022-03-01 17:30:25 +0800

    add randomBigInteger

--- Sources/BigInteger/Number-Theoretic Algorithms.swift
+++ Sources/BigInteger/Number-Theoretic Algorithms.swift
@@ -5,6 +5,8 @@
 //  Created by Louis Shen on 2022/2/28.
 //
 
+import Darwin
+
 extension BigInteger {
     /*
      * Returns a BigInteger whose value is the greatest common divisor of
@@ -27,5 +29,23 @@ extension BigInteger {
         } else {
             return euclid(b, a % b)
         }
+    }
+    
+    /*
+     * Returns a BigInteger whose value consisted of `width` random bits.
+     */
+    public static func
+            randomBigInteger(withMaximumWidth width : Int) -> BigInteger {
+        let count = width / 32
+        let rem = width % 32
+        var mag = [UInt32]()
+        mag.reserveCapacity(count + (rem == 0 ? 0 : 1))
+        for _ in 0 ..< count {
+            mag.append(arc4random())
+        }
+        for _ in count ..< mag.count {
+            mag.append(arc4random() >> (32 - rem))
+        }
+        return BigInteger(signum: true, mag: mag)
     }
 }
