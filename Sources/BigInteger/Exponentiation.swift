45d2ef2564525cf51f21f24d16a8202c49031cf5
Louis Shen <louiseshen@protonmail.com>
2022-03-02 15:07:13 +0800

    add pow()

--- Sources/BigInteger/Exponentiation.swift
+++ Sources/BigInteger/Exponentiation.swift
@@ -7,12 +7,38 @@
 
 extension BigInteger {
     /*
-     *
+     * Returns a BigInteger whose value is a ** b.
      */
-//    public static func pow(_ a : BigInteger, _ b : BigInteger) -> BigInteger {
-//        var res = 1
-//        while b > 0
-//        
-//        return res
-//    }
+    public static func pow(_ a : BigInteger, _ b : BigInteger) -> BigInteger {
+        var a = a
+        var b = b
+        var res = BigInteger.ONE
+        while b > BigInteger.ZERO {
+            if (b & BigInteger.ONE) == BigInteger.ONE {
+                res *= a
+            }
+            a *= a
+            b >>= 1
+        }
+        
+        return res
+    }
+    
+    /*
+     * Returns a BigInteger whose value is a ** b. (where b is Int)
+     */
+    public static func pow(_ a : BigInteger, _ b : Int) -> BigInteger {
+        var a = a
+        var b = b
+        var res = BigInteger.ONE
+        while b > 0 {
+            if (b & 1) == 1 {
+                res *= a
+            }
+            a *= a
+            b >>= 1
+        }
+        
+        return res
+    }
 }
