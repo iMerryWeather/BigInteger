073b12d0b604f7c5f66fe49f66210ae250953252
Louis Shen <louiseshen@protonmail.com>
2022-03-04 17:16:57 +0800

    fast modPow()

--- Sources/BigInteger/Exponentiation.swift
+++ Sources/BigInteger/Exponentiation.swift
@@ -44,19 +44,22 @@ extension BigInteger {
     
     /*
      * Returns a BigInteger whose value is (a ** b) mod n.
+     *
+     * Complexity: O(log b)
      */
     public static func pow(_ a : BigInteger,
                            _ b : BigInteger,
                            _ n : BigInteger) -> BigInteger {
         var a = a % n
-        var b = b
         var res = BigInteger.ONE
-        while b > BigInteger.ZERO {
-            if (b & BigInteger.ONE) == BigInteger.ONE {
-                res = res * a % n
+        for var e in b.mag {
+            for _ in 0 ..< 32 {
+                if (e & 1) == 1 {
+                    res = res * a % n
+                }
+                a = a * a % n
+                e >>= 1
             }
-            a = a * a % n
-            b >>= 1
         }
         
         return res
